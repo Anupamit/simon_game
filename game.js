0033d1ffeb8de81109e91971934dbdc5fbcb17c2
Anupam Kumar <anupamit011@gmail.com>
2020-06-29 11:11:34 +0530

    js file

--- game.js
+++ game.js
@@ -1,19 +1,43 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
+var userClickedPattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+});
+
 function nextSequence() {
 
-  //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
   var randomNumber = Math.floor(Math.random() * 4);
-
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
   var randomChosenColour = buttonColours[randomNumber];
-
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
   gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
